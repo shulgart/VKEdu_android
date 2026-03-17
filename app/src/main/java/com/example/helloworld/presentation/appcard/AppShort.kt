9aa3badee4e8fafb589e0401c2098ac47dc1e0ac
shulgart <artyom_shulgin@mail.ru>
2026-03-17 21:54:18 +0500

    move AppCategory to separate file

--- app/src/main/java/com/example/helloworld/presentation/appcard/AppShort.kt
+++ app/src/main/java/com/example/helloworld/presentation/appcard/AppShort.kt
@@ -1,20 +1,5 @@
 package com.example.helloworld.presentation.appcard
 
-enum class AppCategory(desc: String){
-    FINANCES("Финансы"),
-    TOOLS("Инструменты"),
-    TRANSPORT("Транспорт");
-
-    val desc: String = ""
-}
-
-fun getAppCategory(app: AppCategory) =
-    when (app){
-        AppCategory.FINANCES -> "Финансы"
-        AppCategory.TOOLS -> "Инструменты"
-        AppCategory.TRANSPORT-> "Транспорт"
-    }
-
 data class AppShort(
     val name: String,
     val description: String,
