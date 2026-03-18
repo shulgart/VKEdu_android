34d09f6bba31dedf302f3c3999bddecdccfaba53
shulgart <artyom_shulgin@mail.ru>
2026-03-18 22:07:59 +0500

    mop up with MainActivity

---
+++ app/src/main/java/com/example/helloworld/domain/appdetails/GetAppUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.helloworld.domain.appdetails
+
+class GetAppDetailsUseCase(
+    private val appDetailsRepository: AppDetailsRepository,
+) {
+    suspend operator fun invoke(): AppDetails {
+        val app: AppDetails = appDetailsRepository.get()
+
+        if (app.category == Category.GAME) {
+            throw IllegalStateException()
+        }
+
+        return app
+    }
+}
